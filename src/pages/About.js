a2263139db9d74636390d1260f11c86a24c8afeb
milymax <vararakya@gmail.com>
2022-04-20 23:37:36 +0700

    first

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+function About() {
+	return (
+		<div>
+			<div className="text-center">
+			<h1 className="mb-4 text-xl font-semibold">About us</h1>
+			<p className="font-light">
+				happy shopping
+			</p>
+			</div>
+		</div>
+	);
+}
+
+export default About;
